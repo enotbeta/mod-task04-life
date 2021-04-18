34a9597409892c5b4bd049f6cd902b9db0c997f0
Sergey Kamskov <sk1999@inbox.ru>
2021-04-18 23:36:57 +0300

    Features implemented

--- Life/Program.cs
+++ Life/Program.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
 
 namespace cli_life
 {
     public class Cell
     {
         public bool IsAlive;
+        public bool IsChecked = false;
         public readonly List<Cell> neighbors = new List<Cell>();
         private bool IsAliveNext;
         public void DetermineNextLiveState()
@@ -24,6 +28,17 @@ namespace cli_life
         {
             IsAlive = IsAliveNext;
         }
+        public void Check()
+        {
+            IsChecked = true;
+            foreach (var neighbour in neighbors)
+            {
+                if (neighbour.IsAlive == true && neighbour.IsChecked == false)
+                {
+                    neighbour.Check();
+                }              
+            }
+        }
     }
     public class Board
     {
@@ -35,7 +50,7 @@ namespace cli_life
         public int Width { get { return Columns * CellSize; } }
         public int Height { get { return Rows * CellSize; } }
 
-        public Board(int width, int height, int cellSize, double liveDensity = .1)
+        [JsonConstructor]public Board(int width, int height, int cellSize, double liveDensity = .1)
         {
             CellSize = cellSize;
 
@@ -46,6 +61,34 @@ namespace cli_life
 
             ConnectNeighbors();
             Randomize(liveDensity);
+        }
+        public Board(string inputText) 
+        {
+            CellSize = 1;
+
+            string[] text = inputText.Split('\n');
+            int textColumns = text[0].Length;
+            int textRows = 1 +(inputText.Length - textColumns) / textColumns;
+            Cells = new Cell[textColumns, textRows];
+
+            Console.WriteLine(Cells.GetLength(0));
+            Console.WriteLine(Cells.GetLength(1));
+            for(int i = 0; i < textRows; i ++)
+            {
+                for(int j = 0; j < textColumns; j++)
+                {
+                    Cells[j,i] = new Cell();
+                    if (text[i][j] == '*')
+                    {
+                        Cells[j,i].IsAlive = true;
+                    }
+                    else
+                    {
+                        Cells[j,i].IsAlive = false;
+                    }
+                }
+            }
+            ConnectNeighbors();
         }
 
         readonly Random rand = new Random();
@@ -85,17 +128,78 @@ namespace cli_life
                 }
             }
         }
+        public void Stats()
+        {
+            int counterFigure = 0;
+            int counterCells = 0;
+            for(int i = 0; i < Rows; i ++)
+            {
+                for(int j = 0; j < Columns; j ++)
+                {
+                    if(Cells[j,i].IsAlive == true)
+                        {
+                            counterCells += 1;
+                            if(Cells[j,i].IsChecked ==  false)
+                                {
+                                    Console.WriteLine("Figure");
+                                    counterFigure += 1;
+                                    Cells[j,i].Check();
+                                }
+                        }
+                    
+                }
+            }
+            Console.WriteLine("Cells "+counterCells);
+            Console.WriteLine("Figures "+counterFigure);
+        }
+        public void Save(string name)
+        {
+            string text = "";
+            for (int row = 0; row < Rows; row++)
+            {
+                for (int col = 0; col < Columns; col++)   
+                {
+                    var cell = Cells[col, row];
+                    if (cell.IsAlive)
+                    {
+                        text += '*';
+                    }
+                    else
+                    {
+                        text += ' ';
+                    }
+                }
+                text += '\n';
+            }
+            File.WriteAllText(".\\"+name+".txt", text);
+
+        }
+
+        
     }
+    
+
+
     class Program
     {
+        static bool Flag = true;
         static Board board;
-        static private void Reset()
+        static private void Reset(string JsonPath = "")
         {
-            board = new Board(
+            if (JsonPath == "")
+            {   
+                board = new Board(
                 width: 50,
                 height: 20,
                 cellSize: 1,
                 liveDensity: 0.5);
+            }
+            else
+            {               
+                Console.WriteLine(File.ReadAllText(JsonPath)) ;
+                Dictionary<string, double> dict = JsonSerializer.Deserialize<Dictionary<string, double>>(File.ReadAllText(JsonPath));
+                board = new Board((int)dict["width"],(int)dict["height"],(int)dict["cellSize"],dict["liveDensity"]);                
+            }
         }
         static void Render()
         {
@@ -116,16 +220,52 @@ namespace cli_life
                 Console.Write('\n');
             }
         }
+        
         static void Main(string[] args)
         {
-            Reset();
-            while(true)
+            Console.WriteLine("Do you want to read from file? y/n");
+            string x = Console.ReadLine();
+            if(x == "y")
+            {
+                Console.WriteLine("Enter File name");
+                string Name = Console.ReadLine();
+                Read(Name);
+            }
+            else
+            {
+                Reset("board.json"); //put your JSON file here
+            }
+                       
+            Thread A = new Thread(WaitButton);
+            A.Start();
+            while(Flag)
             {
                 Console.Clear();
                 Render();
-                board.Advance();
-                Thread.Sleep(1000);
+                board.Advance();                
+                Thread.Sleep(100);
             }
+            board.Stats();
+            Console.WriteLine("Do you want to save? y/n");
+            string y = Console.ReadLine();
+            if(y == "y")
+            {
+                Console.WriteLine("Enter File name");
+                string Name = Console.ReadLine();
+                board.Save(Name);
+            }
+            Thread.Sleep(10000);
+
+        }
+        static void WaitButton()
+        {
+            Console.ReadKey();
+            Flag = false;
+        }
+        static void Read(string Name)
+        {
+            string text = File.ReadAllText(Name);
+            board = new Board(text);
         }
     }
 }
